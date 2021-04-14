70857d0798bc4f950c6f36ceb15fc8dc300fbb48
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-04-14 17:38:06 +0200

    Refactoring word2vec model

--- models/w2v_spellchecker.py
+++ models/w2v_spellchecker.py
@@ -4,58 +4,68 @@ import pandas as pd
 from gensim.models import KeyedVectors
 from tqdm import tqdm
 
+
 def model(filename):
     model = KeyedVectors.load_word2vec_format(
         filename,
         binary=True)
-    
+
     words = model.index2word
     w_rank = {}
-    for i,word in tqdm(enumerate(words)):
+    for i, word in tqdm(enumerate(words)):
         w_rank[word] = i
 
     vocab = w_rank
 
     return model, vocab
 
+
 def words(text): return re.findall(r'\w+', text.lower())
 
-def P(word): 
+
+def P(word):
     "Probability of `word`."
     # use inverse of rank as proxy
     # returns 0 if the word isn't in the dictionary
     return - vocab.get(word, 0)
 
-def correction(vocab, word): 
+
+def correction(vocab, word):
     "Most probable spelling correction for word."
     return max(candidates(vocab, word), key=P)
 
-def candidates(vocab, word): 
+
+def candidates(vocab, word):
     '''Generate possible spelling corrections for word.
     '''
-    return (known([word], vocab) 
-            or known(vocab, edits1(word)) 
-            or known(vocab, edits2(word)) 
+    return (known([word], vocab)
+            or known(vocab, edits1(word))
+            or known(vocab, edits2(word))
             or [word])
 
-def known(vocab, words): 
+
+def known(vocab, words):
     '''The subset of `words` that appear in the dictionary of vocab.'''
     return set(w for w in words if w in vocab)
 
+
 def edits1(word):
     "All edits that are one edit away from `word`."
-    letters    = 'abcdefghijklmnopqrstuvwxyz'
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes = [L + R[1:] for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+    inserts = [L + c + R for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
-def edits2(word): 
+
+def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
+
 if __name__ == "__main__":
-    model, vocab = model("./GoogleNews-vectors-negative300.bin.gz")
+    model, vocab = model(
+        "../pretrained_w2v/GoogleNews-vectors-negative300.bin.gz")
     correction(vocab, "petersbourg")
--- models/word2vec.py
+++ models/word2vec.py
@@ -1,46 +1,67 @@
-import json, nltk, re
+import json
+import nltk
 import pandas as pd
 import numpy as np
 from gensim.models import KeyedVectors
 from gensim.models import Word2Vec
 import gensim.downloader as api
-import nlpaug.augmenter.char as nac
+import re
 from tqdm import tqdm
 
-def word_augmentation(df, n):
-    aug = nac.KeyboardAug()
-    aug_df = pd.DataFrame(columns=['input','target','code'])
-    
-    for i in tqdm(range(len(df))):
-        words = df.iloc[i]
-        for j in range(n):
-            augmented_data = aug.augment(words["input"])
-            aug_df = aug_df.append({ "input": augmented_data, "target": words["target"], "code": words["code"] }, ignore_index=True)
-    
-    return df.append(aug_df)
 
+class W2V:
+    def __init__(self, classes):
+        self.model = Word2Vec(size=300, window=5, min_count=1, workers=2)
+        self.classes = classes
 
-def model(filename):
-    df = pd.read_csv(filename)
-    df['code'] = df['code'].apply(lambda code: code.replace(" ", ""))
+    @staticmethod
+    def softmax(x):
+        denominator = sum([np.exp(i) for i in x.values()])
+        for key, value in x.items():
+            x[key] = np.exp(value) / denominator
+        return x
 
+    def fit(self, sentences):
+        self.model.build_vocab(sentences)
+        self.model.train(sentences, total_examples=self.model.corpus_count,
+                         epochs=300, report_delay=1)
 
-    sentences = []
-    for index, row in tqdm(df.iterrows()):
-        words = nltk.word_tokenize(row['input']) + nltk.word_tokenize(row['target']) + nltk.word_tokenize(row['code'])
-        words = [re.sub("[^A-Za-z']+", ' ', str(word)).lower() for word in words]
-        sentences.append(words)
+    def __call__(self, text):
+        text = text.lower()
+        text = re.split('; |, | \* | \n | > | / | + | _ | -', text)
+        # remove unknown words
+        known_words = []
+        for word in text:
+            if word in self.model.wv.vocab.keys():
+                known_words.append(word)
 
-    # define the model
-    w2v = Word2Vec(size = 300, window=5, min_count = 1, workers = 2)
-    w2v.build_vocab(sentences)
+        if len(known_words) == 0:
+            sims = []
+        else:
+            sims = self.model.most_similar(
+                known_words, topn=10)  # [(str, distance)]
 
-    # train the model on the dataset
-    w2v.train(sentences, total_examples=w2v.corpus_count,
-            epochs=300, report_delay=1)
-    
-    return w2v
+        labels = {}
+        for label in self.classes:
+            labels[label] = 0
+        for i in range(len(sims)):
+            if sims[i][0].upper() in self.classes:
+                labels[sims[i][0].upper()] += 1
 
+        return max(labels.items(), key=lambda x: x[1])[0], self.softmax(labels)
 
-if __name__ == "__main__":
-    model("")
+    def eval(self, dataset):
+        corrects = 0
+        errors = []
+
+        for i in tqdm(range(len(dataset))):
+            text = dataset.iloc[i]["destination"]
+            target = dataset.iloc[i]["code"]
+
+            pred, _ = self(text)
+            if pred == target:
+                corrects += 1
+            else:
+                errors.append((text, target, pred))
+
+        return corrects / len(dataset), errors
