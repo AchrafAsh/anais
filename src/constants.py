2c047e8b670e942c90624dadfac594192f726ccf
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2021-05-12 20:38:17 +0200

    Add db_config

--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,11 @@ constants = {
     "SEED": 42,
     "classes": ['FRIRK', 'FRLEH', 'NLRTM', 'FRBES', 'ESSDR', 'FRMTX', 'BEANR', 'ESBIO', 'FRBOD', 'FRSML']
 }
+
+db_config = {
+    'user': 'ucyecfawgs76hyzs',
+    'password': 'RZ28LUPWAc0cUt294gG4',
+    'host': 'bne6wsrm7ydvshkrioab-mysql.services.clever-cloud.com',
+    'database': 'bne6wsrm7ydvshkrioab',
+    'raise_on_warnings': True
+}
